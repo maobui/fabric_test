e0c23eab2415c075f9a05e53e2acde0024c0d82f
maobui <mao.buidinh@hotmail.com>
2018-12-21 11:32:19 +0700

    - Log crash.

--- app/src/main/java/com/me/bui/fabrictest/MainActivity.kt
+++ app/src/main/java/com/me/bui/fabrictest/MainActivity.kt
@@ -2,11 +2,36 @@ package com.me.bui.fabrictest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
+
+        btnCrash.setOnClickListener { view -> forceCrash(view) }
+        btnException.setOnClickListener { _ -> forceException() }
     }
+
+    fun forceCrash(view: View) {
+        throw RuntimeException("This is a crash")
+    }
+
+    fun forceException() {
+        try {
+            val x = 1/0
+        } catch (e: Exception) {
+            Crashlytics.logException(e)
+            e.printStackTrace()
+        }
+    }
+
 }
